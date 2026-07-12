c57121ca6524435471c36df13edc06f9cdef7e9c
Kimbsy <lordkimber@gmail.com>
2026-07-12 12:26:47 +0100

    More colour options

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ initial_delay = 20
 glance_delay = 10
 min_delay = 20
 max_delay = 50
-eh = 80
+eh = 85
 ew = 20
 x_gap = 60
 
@@ -19,22 +19,42 @@ positions = [
     [0, 40]
 ]
 
+def parse_hex(s):
+    r = int("0x" + s[1:3], 16)
+    g = int("0x" + s[3:5], 16)
+    b = int("0x" + s[5:7], 16)
+    return [r / 255, g / 255, b / 255]
+
 WHITE = [1, 1, 1]
 BLACK = [0, 0, 0]
 GREY = [0.4, 0.4, 0.4]
 LIGHT_GREY = [0.8, 0.8, 0.8]
-ORANGE = [1, 0.5, 0.1]
+CRAB_ORANGE = [1, 0.5, 0.1]
 DARK_BLUE = [0.17, 0.26, 0.32]
 ICE_BLUE = [0.75, 0.99, 0.98]
 ELECTRIC_PINK = [0.9, 0.01, 0.35]
 ELECTRIC_BLUE = [0.1, 1, 1]
+RED = parse_hex("#E40303")
+ORANGE = parse_hex("#FF8C00")
+YELLOW = parse_hex("#FFED00")
+GREEN = parse_hex("#008026")
+BLUE = parse_hex("#004CFF")
+PURPLE = parse_hex("#732982")
+FOREST_GREEN = parse_hex("#4F9D69")
+BROWN = parse_hex("#504136")
+DARK_PINK = parse_hex("#EF0AFF")
+LIGHT_PINK = parse_hex("#FAADFF")
 
 THEMES = {
     "Monochrome": [WHITE, BLACK],
-    "Crab": [ORANGE, WHITE],
-    "Icy": [ICE_BLUE, BLACK],
+    "Crab": [CRAB_ORANGE, WHITE],
     "Neon": [ELECTRIC_PINK, ELECTRIC_BLUE],
-    "Noir": [GREY, LIGHT_GREY]
+    "Noir": [GREY, LIGHT_GREY],
+    "Desert": [YELLOW, WHITE],
+    "Forest": [FOREST_GREEN, BROWN],
+    "Rose": [LIGHT_PINK, DARK_PINK],
+    "Spiderman": [RED, BLUE],
+    "Pride": ["pride", WHITE]
 }
 
 # @TODO: blinking animation
@@ -85,10 +105,27 @@ class EyesApp(App):
             self.delay_callback()
         self.remaining = self.remaining - 1
 
+    def draw_pride_background(ctx):
+        colour(ctx, RED)
+        ctx.rectangle(-120, -120, 240, 240).fill()
+        colour(ctx, ORANGE)
+        ctx.rectangle(-120, -80, 240, 240).fill()
+        colour(ctx, YELLOW)
+        ctx.rectangle(-120, -40, 240, 240).fill()
+        colour(ctx, GREEN)
+        ctx.rectangle(-120, -0, 240, 240).fill()
+        colour(ctx, BLUE)
+        ctx.rectangle(-120, 40, 240, 240).fill()
+        colour(ctx, PURPLE)
+        ctx.rectangle(-120, 80, 240, 240).fill()
+
     def draw_face(self, ctx):
         # background
-        colour(ctx, self.face_colour)
-        ctx.rectangle(-120, -120, 240, 240).fill()
+        if self.face_colour == "pride":
+            draw_pride_background(ctx)
+        else:
+            colour(ctx, self.face_colour)
+            ctx.rectangle(-120, -120, 240, 240).fill()
 
         colour(ctx, self.eye_colour)
         # draw left eye
